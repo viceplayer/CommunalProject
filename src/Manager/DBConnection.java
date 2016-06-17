707716cf93d8f93663f525c1d1b943239b4591d4
viceplayer <givi.beridze@yahoo.com>
2016-06-17 16:34:52 +0400

    Implemented DBConnection class

--- src/Manager/DBConnection.java
+++ src/Manager/DBConnection.java
@@ -1,5 +1,58 @@
 package Manager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
 
+	public static final String MYSQL_USERNAME = "root";
+	public static final String MYSQL_PASSWORD = "root";
+	public static final String MYSQL_DATABASE_SERVER = "jdbc:mysql://127.0.0.1:3306";
+	public static final String MYSQL_DATABASE_NAME = "oop";
+	public static Connection connection;
+
+	// This class just connects to database
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+		}
+
+		System.out.println("MySQL JDBC Driver Registered!");
+
+		try {
+			connection = DriverManager.getConnection(MYSQL_DATABASE_SERVER + "/" + MYSQL_DATABASE_NAME, MYSQL_USERNAME,
+					MYSQL_PASSWORD);
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+
+	// Return active connection
+	public static Connection getConnection() {
+		return connection;
+	}
+
+	public static void CloseConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+//	public static void main(String[] args) {
+//		DBConnection n = new DBConnection();
+//	}
 }
