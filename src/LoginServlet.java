73ea535ac5fdaefb62606ffac2ce48fff5c944f0
Eirenne <georgewiklauri@gmail.com>
2016-06-19 16:31:15 +0400

    Added servlets

---
+++ src/LoginServlet.java
@@ -0,0 +1,64 @@
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Manager.AccountManager;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Manager.AccountManager am = (Manager.AccountManager) request.getServletContext()
+				.getAttribute("Account Manager");
+		String personalId = request.getParameter("personalId");
+		String password = request.getParameter("password");
+		String path = "";
+		if (am.logIn(personalId, password)) {
+			path += "Home.jsp";
+			System.out.println("Success! Welcome");
+		}else{
+			path += "Home.jsp";
+			System.out.println("Wrong input");
+		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(path);
+        dispatch.forward(request,response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
