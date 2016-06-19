6f33e4e513c527997235f3b0e030bb28c5c54b46
viceplayer <givi.beridze@yahoo.com>
2016-06-19 19:22:18 +0400

    Created Card class

---
+++ src/Manager/Card.java
@@ -0,0 +1,34 @@
+package Manager;
+
+public class Card {
+	private int userId;
+	private String cardNumber;
+	private String date;
+	private String firstName;
+	private String lastName;
+	
+	public Card(int userId, String cardNumber, String date, String firstName, String lastName){
+		this.cardNumber = cardNumber;
+		this.userId = userId;
+		this.date = date;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getCardNumber(){
+		return cardNumber;
+	}
+	public int getUserId(){
+		return userId;
+	}
+	public String getDate(){
+		return date;
+	}
+	public String getFirstName(){
+		return firstName;
+	}
+	public String getLastName(){
+		return lastName;
+	}
+	
+}
