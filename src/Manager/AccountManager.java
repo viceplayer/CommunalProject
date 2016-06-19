39df249f5fa2ad4cab0ce9bf41cf6672e4db3c8e
lmach14 <lmach14@freeuni.edu.ge>
2016-06-19 16:01:06 +0400

    implemented  account manager class

--- src/Manager/AccountManager.java
+++ src/Manager/AccountManager.java
@@ -1,5 +1,35 @@
 package Manager;
 
 public class AccountManager {
+	private DatabaseRelation db;
+
+	public AccountManager() {
+		db = new DatabaseRelation();
+
+	}
+
+	public boolean createAccount(String personalId, String firstName, String lastName, String date, String mail,
+			String mobile, String password) {
+		if (accountExists(personalId) == false) {
+			DatabaseRelation.createUser(personalId, firstName, lastName, date, mail, mobile, password);
+			return true;
+		}
+		return false;
+	}
+
+	public boolean accountExists(String personalId) {
+
+		if (DatabaseRelation.getUserId(personalId) == -1) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean logIn(String personalId, String password) {
+		if (DatabaseRelation.userExists(personalId, password)) {
+			return true;
+		}
+		return false;
+	}
 
 }
