12ecaaa2aba91b45481e4bbca65b57f584813356
viceplayer <givi.beridze@yahoo.com>
2016-06-19 19:10:00 +0400

    Few little errors changed(mostly names of classes)

--- src/Manager/DatabaseRelation.java
+++ src/Manager/DatabaseRelation.java
@@ -41,7 +41,7 @@ public class DatabaseRelation {
 			ps.setString(1, ""+userId);
 			ResultSet rs = ps.executeQuery();
 			while (rs.next()) {
-				object temp = new object(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4));
+				Object temp = new Object(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4));
 				result.add(temp);
 			}
 		} catch (SQLException e) {
