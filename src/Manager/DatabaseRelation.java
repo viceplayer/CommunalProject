55f7ff80e9ef86e1cafa7aa6e5b41558a3c90dba
lmach14 <lmach14@freeuni.edu.ge>
2016-06-19 15:51:32 +0400

    Added methods in DBRelation and some classes

--- src/Manager/DatabaseRelation.java
+++ src/Manager/DatabaseRelation.java
@@ -112,5 +112,67 @@ public class DatabaseRelation {
 		}
 
 	}
+	
+	public static ArrayList<Ticket> getTickets(int objectId) {
+		String query = "SELECT objectId,ticket,companyName FROM ticket a join company b on a.companyId = b.id WHERE objectId = "
+				+ objectId;
+		ArrayList<Ticket> result = new ArrayList<Ticket>();
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Ticket temp = new Ticket(rs.getInt(1), rs.getString(3), rs.getString(2));
+				result.add(temp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+
+	}
+
+	public static ArrayList<Transaction> getTransactions(ArrayList<object> arr) {
+		String query = "SELECT cardNumber,companyName,amount, transactionDate FROM transaction a join company b join card c join object d on a.companyId = b.id AND a.cardId = c.id AND a.objectId = d.id WHERE objectId = ";
+		for(int i=0;i<arr.size();i++){
+			query += arr.get(i).getId();
+			if(i!=arr.size()-1){
+				query += " OR objectId = ";
+			}
+		}
+		ArrayList<Transaction> result = new ArrayList<Transaction>();
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Transaction temp = new Transaction(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDate(4));
+				result.add(temp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+
+	}
+	
+	public static ArrayList<card> getCards(int userId) {
+		String query = "SELECT userId, cardNumber, cardDate, firstName, lastName from card where userId = "
+				+ userId;
+		ArrayList<card> result = new ArrayList<card>();
+		try {
+			PreparedStatement ps = con.prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				card temp = new card(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5));
+				result.add(temp);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+
+	}
 
 }
