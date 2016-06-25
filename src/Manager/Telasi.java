73385691045db6750ab1008f1a4b25056b5b805a
lmach14 <lmach14@freeuni.edu.ge>
2016-06-25 13:30:36 +0400

    Telasi add some methods

--- src/Manager/Telasi.java
+++ src/Manager/Telasi.java
@@ -8,52 +8,64 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 public class Telasi {
-	
+
 	private String url = "http://my.telasi.ge/customers/info/";
 	private static String content = null;
 	private static URLConnection connection = null;
-	
-	public Telasi(int TicketNum){
-		url = url + TicketNum;
-		System.out.println(content);
-//		try {
-//			System.out.println(getUrlSource(url));
-//			
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		
-	}
-	
-	private static void getUrl(String url){
-		try {
-			  connection =  new URL(url).openConnection();
-			  Scanner scanner = new Scanner(connection.getInputStream());
-			  scanner.useDelimiter("\\Z");
-			  content = scanner.next();
-			  scanner.close();
-			}catch ( Exception ex ) {
-			    ex.printStackTrace();
-			}
-	} 
-	
-	private static String getUrlSource(String url) throws IOException {
-        URL web = new URL(url);
-        URLConnection wc = web.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                wc.getInputStream(), "UTF-8"));
-        String inputLine;
-        StringBuilder a = new StringBuilder();
-        while ((inputLine = in.readLine()) != null)
-            a.append(inputLine);
-        in.close();
 
-        return a.toString();
-    }
+	public Telasi(int TicketNum) {
+		url = url + TicketNum;
+		getUrl(url);
+
+	}
+
+	private static String getTicketInfo() {
+		String result = "<ul ";
+		int start = 0;
+		String[] tokens = content.split("[\\s]");
+		for (String s : tokens) {
+			if (s.equals("class=\"list-group\">")) {
+				start = 1;
+			}
+			if (start == 1 && !s.equals("")) {
+				result = result + " " + s;
+			}
+			if (start == 1 && s.equals("</ul>"))
+				break;
+		}
+		return result;
+
+	}
+
+	private static void getUrl(String url) {
+		try {
+			connection = new URL(url).openConnection();
+			Scanner scanner = new Scanner(connection.getInputStream());
+			scanner.useDelimiter("\\Z");
+			content = scanner.next();
+			scanner.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private static String getUrlSource(String url) throws IOException {
+
+		URL web = new URL(url);
+		URLConnection wc = web.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(wc.getInputStream(), "UTF-8"));
+		String inputLine;
+		StringBuilder a = new StringBuilder();
+		while ((inputLine = in.readLine()) != null)
+			a.append(inputLine);
+		in.close();
+
+		return a.toString();
+	}
+
 	public static void main(String[] args) {
 		Telasi t = new Telasi(192819);
+		System.out.println(t.getTicketInfo());
 	}
-	
 
 }
