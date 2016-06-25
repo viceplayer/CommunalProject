b7239467e6b48102342a23af536c5d4ca1f286f8
lmach14 <lmach14@freeuni.edu.ge>
2016-06-25 12:15:21 +0400

    Telasi connection

---
+++ src/Manager/Telasi.java
@@ -0,0 +1,55 @@
+package Manager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+public class Telasi {
+	
+	private String url = "http://my.telasi.ge/customers/info/";
+	
+	public Telasi(int TicketNum){
+		url = url + TicketNum;
+		String content = null;
+		URLConnection connection = null;
+		try {
+		  connection =  new URL(url).openConnection();
+		  Scanner scanner = new Scanner(connection.getInputStream());
+		  scanner.useDelimiter("\\Z");
+		  content = scanner.next();
+		  scanner.close();
+		}catch ( Exception ex ) {
+		    ex.printStackTrace();
+		}
+		System.out.println(content);
+//		try {
+//			System.out.println(getUrlSource(url));
+//			
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+	}
+	private static String getUrlSource(String url) throws IOException {
+        URL web = new URL(url);
+        URLConnection wc = web.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                wc.getInputStream(), "UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            a.append(inputLine);
+        in.close();
+
+        return a.toString();
+    }
+	public static void main(String[] args) {
+		Telasi t = new Telasi(192819);
+	}
+	
+
+}
