c3c16e71ee4b1b7626eacf0d5d6d93b2de077ae2
lmach14 <lmach14@freeuni.edu.ge>
2016-06-25 12:19:28 +0400

    make tabulation

--- src/Manager/Telasi.java
+++ src/Manager/Telasi.java
@@ -10,20 +10,11 @@ import java.util.Scanner;
 public class Telasi {
 	
 	private String url = "http://my.telasi.ge/customers/info/";
+	private static String content = null;
+	private static URLConnection connection = null;
 	
 	public Telasi(int TicketNum){
 		url = url + TicketNum;
-		String content = null;
-		URLConnection connection = null;
-		try {
-		  connection =  new URL(url).openConnection();
-		  Scanner scanner = new Scanner(connection.getInputStream());
-		  scanner.useDelimiter("\\Z");
-		  content = scanner.next();
-		  scanner.close();
-		}catch ( Exception ex ) {
-		    ex.printStackTrace();
-		}
 		System.out.println(content);
 //		try {
 //			System.out.println(getUrlSource(url));
@@ -34,6 +25,19 @@ public class Telasi {
 //		}
 		
 	}
+	
+	private static void getUrl(String url){
+		try {
+			  connection =  new URL(url).openConnection();
+			  Scanner scanner = new Scanner(connection.getInputStream());
+			  scanner.useDelimiter("\\Z");
+			  content = scanner.next();
+			  scanner.close();
+			}catch ( Exception ex ) {
+			    ex.printStackTrace();
+			}
+	} 
+	
 	private static String getUrlSource(String url) throws IOException {
         URL web = new URL(url);
         URLConnection wc = web.openConnection();
