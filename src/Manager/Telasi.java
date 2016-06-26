e672373b8b9a78d2910c6f7ad3ee423765e0ee1f
lmach14 <lmach14@freeuni.edu.ge>
2016-06-26 19:30:54 +0400

    Telasi change url

--- src/Manager/Telasi.java
+++ src/Manager/Telasi.java
@@ -8,28 +8,33 @@ import java.util.Scanner;
 
 public class Telasi {
 
-	private String url = "http://my.telasi.ge/customers/info/";
+	private String url = "http://my.telasi.ge/customers/search?utf8=%E2%9C%93&accnumb=";
+	private String endUrl = "&commit=ძებნა";
 	private static String content = null;
 	private static URLConnection connection = null;
 
 	public Telasi(int TicketNum) {
-		url = url + TicketNum;
+		url = url + TicketNum + endUrl;
 		getUrl(url);
 
 	}
-
+	
+	
 	public static String getTicketInfo() {
 		String start = "class=\"list-group\">";
 		String end = "</ul>";
 		String result = "<ul";
 		int started = 0;
+		boolean  valid = true;
 		String[] tokens = content.split("[\\s]");
 		for (String s : tokens) {
+			if (s.equals("<a")) valid = false;
+			if (s.equals("</div>")) valid = true;
 			if (s.equals(start)) {
 				started = 1;
 			}
 			if (started == 1 && !s.equals("")) {
-				result = result + " " + s;
+				if(valid) result = result + " " + s;
 			}
 			if (started == 1 && s.equals(end))
 				break;
@@ -37,6 +42,7 @@ public class Telasi {
 		return result;
 
 	}
+	
 
 	private static void getUrl(String url) {
 		try {
@@ -62,6 +68,7 @@ public class Telasi {
 			}
 			if (started == 1 && !s.equals("")) {
 				result = result + " " + s;
+				System.err.println(s);
 			}
 			if (started == 1 && s.equals(end))
 				break;
@@ -73,7 +80,7 @@ public class Telasi {
 
 
 //	public static void main(String[] args) {
-//		Telasi t = new Telasi(192819);
+//		Telasi t = new Telasi(3691785);
 //		System.out.println(t.getTicketInfo());
 //		System.out.println(t.getNameAndNumber());
 //	}
