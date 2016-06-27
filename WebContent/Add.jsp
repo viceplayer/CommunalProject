5428221844b2fdbb8c5519a2768e6e3774f2daf5
Eirenne <georgewiklauri@gmail.com>
2016-06-27 16:33:17 +0400

    More css and add button

--- WebContent/Add.jsp
+++ WebContent/Add.jsp
@@ -4,6 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="css/Form.css">
 <title>Insert title here</title>
 </head>
 <body>
--- WebContent/scripts.js
+++ WebContent/scripts.js
@@ -2,11 +2,21 @@
 $(document).ready(function(){
 	var folderManager = $('.folderManager')
 	var data = $('.data')
-
+	
+	
+	$("<li>").text("Add").addClass("Add").attr("href", "http://localhost:8080/CommunalProject/Add.jsp").appendTo(data);
 	$.get("ObjectServlet", function(responseJson) {      
         $.each(responseJson, function(index, Object) {    
         	$("<li>").text(Object.name).addClass(""+Object.type).appendTo(data);
         });
     });
 	
+	
+	
+	$( ".Add" ).on( "click", function() {
+		window.location.href = ("http://localhost:8080/CommunalProject/Add.jsp");
+	});
+	
+	
 });
+
