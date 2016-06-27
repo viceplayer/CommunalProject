480906ce46a1d0a0fd0f0a56410261a79b3ca017
lmach14 <lmach14@freeuni.edu.ge>
2016-06-27 16:44:44 +0400

    DatabaseRelation add hash code

--- src/Manager/DatabaseRelation.java
+++ src/Manager/DatabaseRelation.java
@@ -297,4 +297,20 @@ public class DatabaseRelation {
 		}
 		return result;
 	}
+	
+	/*
+	 * Given a byte[] array, produces a hex String, such as "234a6f". with 2
+	 * chars for each byte in the array. (provided code)
+	 */
+	public static String hexToString(byte[] bytes) {
+		StringBuffer buff = new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+			int val = bytes[i];
+			val = val & 0xff; // remove higher bits, sign
+			if (val < 16)
+				buff.append('0'); // leading 0
+			buff.append(Integer.toString(val, 16));
+		}
+		return buff.toString();
+	}
 }
