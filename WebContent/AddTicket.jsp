b963c525051b8027fd58c6faf834099d05d348fc
viceplayer <givi.beridze@yahoo.com>
2016-06-27 17:19:37 +0400

    Added AddTicket

--- WebContent/AddTicket.jsp
+++ WebContent/AddTicket.jsp
@@ -1,12 +1,24 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="css/Form.css">
+
+<title>Add Ticket</title>
 </head>
 <body>
+	<form action="AddTicketServlet" method="post">
+		Company:
+		<select name="type">
+			<option value="0">Telasi</option>
+			<option value="1">GWP</option>
+			<option value="2">Junk</option>
+		</select> <input type="submit" value="Submit"><br>
+		Ticket Number:<br> <input type="text" name="ticketNumber">
+		
+	</form>
 
 </body>
 </html>
--- src/AddTicketServlet.java
+++ src/AddTicketServlet.java
@@ -1,11 +1,16 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Manager.DatabaseRelation;
 
 /**
  * Servlet implementation class AddTicketServlet
@@ -34,7 +39,26 @@ public class AddTicketServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Manager.AccountManager am = (Manager.AccountManager) request.getServletContext()
+				.getAttribute("Account Manager");
+		String path = "";
+		String ticketNumber = request.getParameter("ticketNumber");
+		int companyId = Integer.parseInt(request.getParameter("companyId"));
+		HttpSession session = request.getSession();
+		int userId = (int)session.getAttribute("userId");
+		// aq unda shevamowmot ticketi ari tu ara objectshi
+		DatabaseRelation.createTicket(objectId, companyId, ticketNumber);
+
+		//		if (!am.addObject(userId, type, name)){
+//			path += "ObjectAlreadyExists.jsp";
+//			System.out.print("Object already exists");
+//		} else {
+//			path += "Home.jsp";
+//			System.out.print("Success! Object Added");
+//
+//		}
+		RequestDispatcher dispatch = request.getRequestDispatcher(path);
+		dispatch.forward(request, response);
 		doGet(request, response);
 	}
 
